970e301e6985a6e437cbb1b783faf3bd9cba9a52
nbovee <12849851+nbovee@users.noreply.github.com>
2023-04-24 17:29:31 -0400

    outline

---
+++ gr_pinball.ino
@@ -0,0 +1,28 @@
+#include <IOdef.h>
+#include <PopBumper.h>
+#include <Flipper.h>
+#include <IMU.h>
+
+
+void setup() {
+
+}
+
+void loop() {
+    update_inputs();
+    game_step();
+    update_outputs();
+}
+
+void game_step(){
+
+}
+
+void update_inputs() {
+
+}
+
+void update_outputs() {
+
+}
+
